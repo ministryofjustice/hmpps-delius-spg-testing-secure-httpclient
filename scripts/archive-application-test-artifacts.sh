815efa532c6941f6b7e12a222b482dab4281d209
Semenu Aggor <semenu.aggor@solirius.com>
2020-07-08 14:33:31 +0100

    ALS-784 Update script - delete command to archive cucumber report as not needed in this project

--- scripts/archive-application-test-artifacts.sh
+++ scripts/archive-application-test-artifacts.sh
@@ -6,6 +6,3 @@ now=$(date +"%m-%d-%Y")
 
 aws s3 cp build/test-results s3://${test_results_bucket}/spgw/${now}/${project_name}/build-${CODEBUILD_BUILD_NUMBER}-unit-test-results  --recursive
 aws s3 cp build/reports/tests s3://${test_results_bucket}/spgw/${now}/${project_name}/build-${CODEBUILD_BUILD_NUMBER}-unit-test-reports --recursive
-
-aws s3 cp build/cucumber s3://${test_results_bucket}/spgw/${now}/${project_name}/build-${CODEBUILD_BUILD_NUMBER}-integration-test-results --recursive
-aws s3 cp build/cucumber-html-reports s3://${test_results_bucket}/spgw/${now}/${project_name}/build-${CODEBUILD_BUILD_NUMBER}-integration-test-reports --recursive
